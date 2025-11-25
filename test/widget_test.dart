af7d0b4f03a6decf15642ce648fe6fe587aec6a5
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-11-25 18:36:41 +0330

    change app_name and package_name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hotelino/main.dart';
+import 'package:Hotelino/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
