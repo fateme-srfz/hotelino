f9dfa8c9bf96fcb4ad4d4651e6c6f401ec0973b2
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-11-30 20:04:28 +0330

    add update systemUI code

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ThemeProvider extends ChangeNotifier {
   Brightness _brightness;
@@ -8,7 +9,7 @@ class ThemeProvider extends ChangeNotifier {
 
   void updateBrightness(Brightness brightness) {
     _brightness = brightness;
-
+    _updateSystemUI();
     notifyListeners();
   }
 
@@ -17,6 +18,22 @@ class ThemeProvider extends ChangeNotifier {
         ? Brightness.light
         : Brightness.dark;
 
+    _updateSystemUI();
     notifyListeners();
+  }
+
+  void _updateSystemUI() {
+    bool isDark = _brightness == Brightness.dark;
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
+        systemNavigationBarColor: isDark ? Colors.black : Colors.white,
+        systemNavigationBarIconBrightness: isDark
+            ? Brightness.light
+            : Brightness.dark,
+      ),
+    );
   }
 }
