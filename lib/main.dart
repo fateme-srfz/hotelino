14892c5f4f3dd8384862889e57dfa2d83bb35354
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-11-30 18:08:13 +0330

    add multiprovider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:Hotelino/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(
+            WidgetsBinding.instance.platformDispatcher.platformBrightness,
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
