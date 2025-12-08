b7e3a80b231c8b41eaf9dd8b71b0625f19340c64
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-08 13:17:20 +0330

    add profile_data_claa and profile_repository

---
+++ lib/features/home/data/repositories/profile_repository.dart
@@ -0,0 +1,19 @@
+import 'package:Hotelino/features/home/data/models/profile.dart';
+
+class ProfileRepository {
+  Future<Profile> fetchUserProfile() async {
+    await Future.delayed(Duration(milliseconds: 100));
+    return Profile(
+      id: "7954862145",
+      name: "کیان خاکی",
+      email: "kiankhaki@gmail.com",
+      avatarUrl: "https://dunijet.ir/content/projects/hotelino/profile_pic.png",
+      phoneNumber: "+989123456789",
+      location: "تهران، ایران",
+      bio: "عاشق سفر و تجربه هتل‌های لاکچری 🌍✨",
+      bookings: 12,
+      favorites: 5,
+      notifications: 3,
+    );
+  }
+}
