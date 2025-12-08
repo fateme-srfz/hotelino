a4ac9757d6629cf396eb7b747807fe65f9c5d58a
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-08 12:06:15 +0330

    add filters and story sections in home provider

--- lib/features/home/presentation/provider/home_provider.dart
+++ lib/features/home/presentation/provider/home_provider.dart
@@ -1,3 +1,5 @@
+import 'package:Hotelino/core/constants/constants.dart';
+import 'package:Hotelino/features/home/data/models/homepage_data.dart';
 import 'package:Hotelino/features/home/data/models/hotel.dart';
 import 'package:Hotelino/features/home/data/repositories/hotel_repository.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +13,47 @@ class HomeProvider extends ChangeNotifier {
   List<Hotel> _hotels = [];
   List<Hotel> get hotels => _hotels;
 
+  final HomepageData _homepageData = HomePageDataConstants.homePageData;
+  HomepageData get homePageData => _homepageData;
+
   fetchHotels() async {
     _hotels = await _hotelRepository.fetchHotels();
     notifyListeners();
   }
+
+  //Filter Methods- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  List<Hotel> getPopularHotels() {
+    return _hotels
+        .where((hotel) => _homepageData.popular.contains(hotel.id))
+        .toList();
+  }
+
+  List<Hotel> getSpecialOffersHotels() {
+    return _hotels
+        .where((hotel) => _homepageData.specialOffers.contains(hotel.id))
+        .toList();
+  }
+
+  List<Hotel> getNewestHotels() {
+    return _hotels
+        .where((hotel) => _homepageData.newest.contains(hotel.id))
+        .toList();
+  }
+
+  //Story Section - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  List<String> getStoryImages() {
+    final shuffledHotels = List<Hotel>.from(_hotels)..shuffle();
+
+    return shuffledHotels.take(3).map((hotel) => hotel.images[0]).toList();
+  }
+
+  final List<String> _storyTitles = [
+    "امکانات رفاهی کامل",
+    "اقامت در قلب شهر",
+    "لوکس ترین هتل ها",
+  ];
+
+  List<String> get storyTitles => _storyTitles;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:Hotelino/bootstrap.dart';
 import 'package:Hotelino/core/theme/app_theme.dart';
 import 'package:Hotelino/core/theme/theme_provider.dart';
+import 'package:Hotelino/features/home/data/repositories/hotel_repository.dart';
+import 'package:Hotelino/features/home/presentation/provider/home_provider.dart';
 import 'package:Hotelino/features/onboarding/data/repositories/onboarding_repository.dart';
 import 'package:Hotelino/features/onboarding/presentation/onboarding_provider.dart';
 import 'package:Hotelino/routes/app_route.dart';
+import 'package:Hotelino/shared/services/json_data_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +18,8 @@ void main() async {
   await lazyBootStrap();
   FlutterNativeSplash.remove();
 
+  final hotelRepository = HotelRepository(jsonDataService: JsonDataService());
+
   runApp(
     MultiProvider(
       providers: [
@@ -23,7 +28,11 @@ void main() async {
             WidgetsBinding.instance.platformDispatcher.platformBrightness,
           ),
         ),
-        ChangeNotifierProvider(create: (_) => OnboardingProvider(OnboardingRepository()))
+        ChangeNotifierProvider(
+          create: (_) => OnboardingProvider(OnboardingRepository()),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => HomeProvider(hotelRepository)),
       ],
       child: const MyApp(),
     ),
