9e52e4a2144d5d6969aa4c7a3f74d32aac67cf8f
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-09 11:03:03 +0330

    finish home Appbar

--- lib/features/home/presentation/widgets/home_appbar.dart
+++ lib/features/home/presentation/widgets/home_appbar.dart
@@ -1,10 +1,15 @@
+import 'package:Hotelino/core/theme/theme_provider.dart';
+import 'package:Hotelino/features/home/presentation/provider/profile_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppbar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return AppBar(
       elevation: 0,
       automaticallyImplyLeading: false,
@@ -17,19 +22,59 @@ class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
                 children: [
                   IconButton(
                     onPressed: () {},
-                    icon: Icon(Icons.notifications_none, color: Colors.grey), ), ], ),],),
-
-          Row(
-            children: [
-              Text('کاربر', style: Theme.of(context).textTheme.titleMedium),
-              const SizedBox(width: 8),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage(
-                  "https://www.w3schools.com/howto/img_avatar.png",
+                    icon: Icon(Icons.notifications_none, color: Colors.grey),
+                  ),
+                  Consumer<ProfileProvider>(
+                    builder: (context, profileProvider, child) {
+                      return profileProvider.profile?.notifications != null &&
+                              profileProvider.profile!.notifications > 0
+                          ? Positioned(
+                              top: 14,
+                              right: 9,
+                              child: CircleAvatar(
+                                radius: 4,
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.primary,
+                              ),
+                            )
+                          : SizedBox();
+                    },
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {
+                  themeProvider.toggleTheme();
+                },
+                icon: Icon(
+                  themeProvider.brightness == Brightness.light
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                  color: Colors.grey,
                 ),
               ),
             ],
+          ),
+
+          Consumer<ProfileProvider>(
+            builder: (context, profileProvider, child) {
+              return Row(
+                children: [
+                  Text(
+                    profileProvider.profile?.name ?? "کاربر",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                     profileProvider.profile?.avatarUrl ??  "https://www.w3schools.com/howto/img_avatar.png",
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ],
       ),
