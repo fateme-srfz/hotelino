8ad27faabdf79165f7b7db6d1c7395a95f6dd989
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-13 20:45:43 +0330

    add AnimatedFavoriteButton

---
+++ lib/features/home/presentation/widgets/animated_favorite_button.dart
@@ -0,0 +1,86 @@
+import 'package:Hotelino/shared/widgets/glassmorphism.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedFavoriteButton extends StatefulWidget {
+  final bool isFavorite;
+  final VoidCallback onTap;
+
+  const AnimatedFavoriteButton({
+    super.key,
+    required this.isFavorite,
+    required this.onTap,
+  });
+
+  @override
+  AnimatedFavoriteButtonState createState() => AnimatedFavoriteButtonState();
+}
+
+class AnimatedFavoriteButtonState extends State<AnimatedFavoriteButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.25,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.elasticOut));
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    widget.onTap();
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Glassmorphism(
+        blur: 3,
+        borderRadius: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white60, width: 0.5),
+          ),
+          child: AnimatedBuilder(
+            animation: _scaleAnimation,
+            builder: (context, child) {
+              return Transform.scale(
+                scale: _scaleAnimation.value,
+                child: CircleAvatar(
+                backgroundColor: Colors.white60.withValues(alpha: 10),
+                child: Icon(
+                  Icons.favorite_rounded,
+                  color: widget.isFavorite ? Colors.red : Colors.white,
+                ),
+              ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
