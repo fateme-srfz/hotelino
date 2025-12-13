5fe086d21f5fc02b152905826b15197d9d8d6442
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-13 13:04:27 +0330

    add glassmorphism widget

---
+++ lib/features/home/presentation/widgets/glassmorphism.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Glassmorphism extends StatelessWidget {
+  final Widget child;
+  final double borderRadius;
+  final double blur;
+  final Color borderColor;
+  final Color backgroundColor;
+
+  const Glassmorphism({
+    super.key,
+    required this.child,
+    this.borderRadius = 20.0,
+    this.blur = 10.0,
+    this.borderColor = Colors.white70,
+    this.backgroundColor = Colors.white10,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: blur,sigmaY: blur),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            border: Border.all(
+              color: borderColor.withValues(alpha: 20)
+            ),
+            color: backgroundColor.withValues(alpha: 30)
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
