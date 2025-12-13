7651464f195a3ee8bc06a20a76e69ab69527ea7d
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-13 10:26:21 +0330

    add hotel_card

--- lib/features/home/presentation/widgets/hotel_card.dart
+++ lib/features/home/presentation/widgets/hotel_card.dart
@@ -1,10 +1,103 @@
+import 'package:Hotelino/core/utils/price_formatter.dart';
+import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/home/presentation/provider/favorite_item_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HotelCard extends StatelessWidget {
-  const HotelCard({super.key});
+  Hotel hotel;
+  HotelCard({super.key, required this.hotel});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final favoriteProvider = Provider.of<FavoriteItemProvider>(context);
+    final isFavorite = favoriteProvider.isFavorite(hotel.id);
+
+    return SizedBox(
+      width: 280,
+      child: Card(
+        elevation: 4,
+        margin: const EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(children: [
+               
+            ],),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const SizedBox(width: 8),
+                      Icon(Icons.star, color: Colors.amber, size: 20),
+                      const SizedBox(width: 4),
+                      Text(
+                        "${hotel.rating} (${formatPrice(hotel.reviewCount)})",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Spacer(),
+                      Text(
+                        hotel.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const SizedBox(width: 8),
+                      Text(
+                        "${hotel.city} , ${hotel.country}",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(width: 4),
+                      Icon(
+                        Icons.location_on,
+                        color: Theme.of(context).colorScheme.primary,
+                        size: 18,
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text("از  ${formatPrice(hotel.pricePerNight)} / شب ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8,left: 8),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: (){}, 
+                        child: Text('مشاهده و انتخاب اتاق',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                        )
+                        ),
+                    ),
+                  ),
+                  const SizedBox(height: 8,)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
