1e87ec3313aa67be11386c76886afb3c86edc675
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-14 16:45:40 +0330

    add HotelVerticalList

---
+++ lib/features/home/presentation/widgets/hotel_vertical_list.dart
@@ -0,0 +1,40 @@
+import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/home/presentation/widgets/hotel_card_vertical.dart';
+import 'package:flutter/material.dart';
+
+class HotelVerticalList extends StatelessWidget {
+  final String title;
+  final List<Hotel> hotels;
+
+  const HotelVerticalList({
+    super.key,
+    required this.title,
+    required this.hotels,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: hotels.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16.0),
+              child: HotelCardVertical(hotel: hotels[index]),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
