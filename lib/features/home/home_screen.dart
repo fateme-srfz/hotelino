485a6e3967a92fe3bacecd780d2ca68e62eb9c76
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-14 17:35:16 +0330

    add HotelVerticalList to home_screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:Hotelino/features/home/presentation/provider/home_provider.dart'
 import 'package:Hotelino/features/home/presentation/widgets/ad_banner.dart';
 import 'package:Hotelino/features/home/presentation/widgets/home_appbar.dart';
 import 'package:Hotelino/features/home/presentation/widgets/hotel_list_section.dart';
+import 'package:Hotelino/features/home/presentation/widgets/hotel_vertical_list.dart';
 import 'package:Hotelino/features/home/presentation/widgets/search_bar.dart';
 import 'package:Hotelino/features/home/presentation/widgets/story_carousel.dart';
 import 'package:flutter/material.dart';
@@ -48,6 +49,14 @@ class HomeScreen extends StatelessWidget {
                 return StoryCarousel(
                   images: homeProvider.getStoryImages(), 
                   titles: homeProvider.storyTitles);
+              },
+            ),
+            Consumer<HomeProvider>(
+              builder: (context, homeProvider, child) {
+                return HotelVerticalList(
+                  title: 'جدیدترین هتل ها',
+                  hotels: homeProvider.getNewestHotels(),
+                );
               },
             ),
           ],
