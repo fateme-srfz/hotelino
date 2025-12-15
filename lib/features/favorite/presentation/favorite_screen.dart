9f424f7be59e693a3fff146586bf8fb308f30975
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-15 14:15:25 +0330

    add FavoriteHotelCard to favorite_screen

--- lib/features/favorite/presentation/favorite_screen.dart
+++ lib/features/favorite/presentation/favorite_screen.dart
@@ -1,10 +1,55 @@
+import 'package:Hotelino/features/favorite/presentation/widgets/favorite_item.dart';
+import 'package:Hotelino/features/home/presentation/provider/favorite_item_provider.dart';
+import 'package:Hotelino/features/home/presentation/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'هتل های مورد علاقه',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(height: 16),
+            const SearchBarWidget(),
+            const SizedBox(height: 16),
+            Consumer<FavoriteItemProvider>(
+              builder: (context, favoriteProvider, child) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: favoriteProvider.favoriteHotelList.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 6,
+                      ),
+                      child: FavoriteHotelCard(
+                        hotel: favoriteProvider.favoriteHotelList[index],
+                        onRemoveFavoriteClicked: (hotelId) {
+                          favoriteProvider.toggleFavoriteHotels(hotelId);
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
