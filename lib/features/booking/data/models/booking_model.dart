5846420df9c8af65c5f688ffbd4b978273e010d2
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-15 19:34:48 +0330

    add booking data classes

---
+++ lib/features/booking/data/models/booking_model.dart
@@ -0,0 +1,19 @@
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
+
+class BookingModel {
+  String fullName;
+  String destination;
+  JalaliRange? checkInOutRangeDate;
+  String numberOfGuests;
+  String phoneNumber;
+
+  BookingModel(
+    {
+      this.fullName ='',
+      this.destination = '',
+      this.checkInOutRangeDate,
+      this.numberOfGuests ='',
+      this.phoneNumber = '',
+    }
+  );
+}
