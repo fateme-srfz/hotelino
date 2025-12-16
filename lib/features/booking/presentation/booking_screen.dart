7b019866e9649946d6a9e8c5efb81bd6040da55c
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-16 14:14:11 +0330

    add resetForm Function to booking_screen

--- lib/features/booking/presentation/booking_screen.dart
+++ lib/features/booking/presentation/booking_screen.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class BookingScreen extends StatelessWidget {
+class BookingScreen extends StatefulWidget {
   const BookingScreen({super.key});
+
+  @override
+  State<BookingScreen> createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  void resetForm() {
+    Future.delayed(const Duration(milliseconds: 100), () {
+      _formKey.currentState?.reset();
+      setState(() {
+        
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
