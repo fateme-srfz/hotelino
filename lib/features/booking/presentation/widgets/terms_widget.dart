5be825887d158d8c82620b52cc7b154e0e81ef16
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-28 13:32:01 +0330

    fix: add unfocusditors to showTermsDialog in terms_widget

--- lib/features/booking/presentation/widgets/terms_widget.dart
+++ lib/features/booking/presentation/widgets/terms_widget.dart
@@ -1,4 +1,5 @@
 import 'package:Hotelino/core/constants/constants.dart';
+import 'package:Hotelino/core/utils/keyboard.dart';
 import 'package:flutter/material.dart';
 
 class TermsWidget extends StatefulWidget {
@@ -125,6 +126,7 @@ class _TermsWidgetState extends State<TermsWidget> {
   }
 
   void _showTermsDialog(BuildContext context) {
+    unfocusEditors(context);
     showModalBottomSheet(
       context: context,
       builder: (context) {
