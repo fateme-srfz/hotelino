c4139ded9c7ade14fb2a0a07a3d963448375903a
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-29 19:58:19 +0330

    add circleAvatar to profile_screen

--- lib/features/home/data/repositories/profile_repository.dart
+++ lib/features/home/data/repositories/profile_repository.dart
@@ -7,7 +7,8 @@ class ProfileRepository {
       id: "7954862145",
       name: "کیان خاکی",
       email: "kiankhaki@gmail.com",
-      avatarUrl: "https://dunijet.ir/content/projects/hotelino/profile_pic.png",
+      avatarUrl:
+          "https://old.dunijet.ir/content/projects/hotelino/profile_pic.png",
       phoneNumber: "+989123456789",
       location: "تهران، ایران",
       bio: "عاشق سفر و تجربه هتل‌های لاکچری 🌍✨",
--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:Hotelino/features/home/presentation/provider/profile_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -27,6 +29,57 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ProfileProvider>(
+      builder: (context, profileProvider, child) {
+        final profile = profileProvider.profile;
+        if (profile == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Scaffold(
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 24),
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 55,
+                        backgroundImage: _image != null
+                            ? FileImage(_image!)
+                            : NetworkImage(profile.avatarUrl)
+                                  as ImageProvider,
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: GestureDetector(
+                          onTap: _pickImage,
+                          child: Container(
+                            padding: const EdgeInsetsDirectional.all(6),
+                            decoration: BoxDecoration(
+                              color: Theme.of(
+                                context,
+                              ).colorScheme.primaryFixed,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.edit,
+                              size: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
