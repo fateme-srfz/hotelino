9ed1db78891581b66bc7acf8cc013d2cd9ec14c6
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-29 20:44:45 +0330

    add profile_option_item

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:Hotelino/features/home/presentation/provider/profile_provider.dart';
+import 'package:Hotelino/features/profile/presentation/widgets/profile_option_item.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
@@ -25,6 +26,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
         _image = File(picked.path);
       });
     }
+  }
+
+  void _showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message, textDirection: TextDirection.rtl)),
+    );
   }
 
   @override
@@ -83,9 +90,42 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       context,
                     ).textTheme.headlineSmall!.copyWith(color: Colors.grey),
                   ),
-                  const SizedBox(height: 24,),
-                   
-
+                  const SizedBox(height: 24),
+                  ProfileOptionItem(
+                    title: "اطلاعات صفحه کاربری",
+                    icon: Icons.person_outline,
+                    onTap: () =>
+                        _showSnackbar(context, "مشاهده اطلاعات صفحه کاربری"),
+                  ),
+                  ProfileOptionItem(
+                    title: "اعلان ها",
+                    icon: Icons.notifications_outlined,
+                    onTap: () => _showSnackbar(context, "مشاهده اعلان‌ها"),
+                  ),
+                  ProfileOptionItem(
+                    title: "لیست مورد علاقه ها",
+                    icon: Icons.favorite_outline,
+                    onTap: () =>
+                        _showSnackbar(context, "مشاهده لیست علاقه‌مندی‌ها"),
+                  ),
+                  ProfileOptionItem(
+                    title: "فراموشی رمز عبور",
+                    icon: Icons.key_outlined,
+                    onTap: () =>
+                        _showSnackbar(context, "تغییر یا بازیابی رمز عبور"),
+                  ),
+                  ProfileOptionItem(
+                    title: "روش های پرداخت",
+                    icon: Icons.credit_card_outlined,
+                    onTap: () =>
+                        _showSnackbar(context, "مشاهده روش‌های پرداخت"),
+                  ),
+                  ProfileOptionItem(
+                    title: "تنظیمات",
+                    icon: Icons.settings_outlined,
+                    onTap: () =>
+                        _showSnackbar(context, "تنظیمات پروفایل کاربری"),
+                  ),
                 ],
               ),
             ),
---
+++ lib/features/profile/presentation/widgets/profile_option_item.dart
@@ -0,0 +1,49 @@
+import 'package:Hotelino/core/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfileOptionItem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+  const ProfileOptionItem({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8, top: 8),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 15),
+          child: Row(
+            children: [
+              const Icon(Icons.arrow_back_ios_new_rounded, size: 14),
+              Expanded(
+                child: Text(
+                  title,
+                  textAlign: TextAlign.right,
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+              ),
+              const SizedBox(width: 12),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: const Color(0xFFF4EAE2),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Icon(icon, color: AppColors.darkFocusedBorder, size: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
