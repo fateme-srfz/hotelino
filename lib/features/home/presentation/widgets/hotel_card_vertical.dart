271ed741866c4c7ba7c732cbac47d0b5f9e0b55e
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-30 15:38:19 +0330

    feat : go to hotelDetailScreen by clicking on HotelCardVertical

--- lib/features/home/presentation/widgets/hotel_card_vertical.dart
+++ lib/features/home/presentation/widgets/hotel_card_vertical.dart
@@ -1,7 +1,9 @@
 import 'package:Hotelino/core/utils/network.dart';
 import 'package:Hotelino/core/utils/price_formatter.dart';
 import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/hotel_detail/presentation/hotel_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class HotelCardVertical extends StatelessWidget {
   final Hotel hotel;
@@ -9,105 +11,115 @@ class HotelCardVertical extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Theme.of(context).colorScheme.surface,
-        boxShadow: [
-          if (Theme.of(context).brightness == Brightness.light)
-            BoxShadow(
-              color: Colors.grey.shade300,
-              blurRadius: 6,
-              spreadRadius: 2,
-            )
-          else
-            BoxShadow(
-              color: Theme.of(
-                context,
-              ).colorScheme.surfaceContainerHigh.withValues(alpha: 1),
-              blurRadius: 6,
-              spreadRadius: 2,
-            ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('رزرو اتاق', style: TextStyle(color: Colors.white)),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const SizedBox(height: 4),
-                  Text(
-                    hotel.name,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text('${hotel.city}, ${hotel.country}'),
-                      const SizedBox(width: 5),
-                      Icon(
-                        Icons.location_on,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 18,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 4),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      // Text("${hotel.bedType.count}, ${hotel.bedType.type}"),
-                      Text(
-                        " ${hotel.bedType.count} ${hotel.bedType.details}",
-                        textDirection: TextDirection.rtl,
-                      ),
-                      const SizedBox(width: 5),
-                      Icon(
-                        Icons.bed,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 18,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'از ${formatPrice(hotel.pricePerNight)} / شب',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 4),
-                ],
+    return GestureDetector(
+      onTap: () {
+        PersistentNavBarNavigator.pushNewScreen(
+          context,
+          screen: HotelDetailScreen(hotelId: hotel.id),
+          withNavBar: true,
+          pageTransitionAnimation: PageTransitionAnimation.cupertino
+        );
+      },
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Theme.of(context).colorScheme.surface,
+          boxShadow: [
+            if (Theme.of(context).brightness == Brightness.light)
+              BoxShadow(
+                color: Colors.grey.shade300,
+                blurRadius: 6,
+                spreadRadius: 2,
+              )
+            else
+              BoxShadow(
+                color: Theme.of(
+                  context,
+                ).colorScheme.surfaceContainerHigh.withValues(alpha: 1),
+                blurRadius: 6,
+                spreadRadius: 2,
+              ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('رزرو اتاق', style: TextStyle(color: Colors.white)),
               ),
             ),
-          ),
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(15),
-              bottomRight: Radius.circular(15),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(height: 4),
+                    Text(
+                      hotel.name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text('${hotel.city}, ${hotel.country}'),
+                        const SizedBox(width: 5),
+                        Icon(
+                          Icons.location_on,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        // Text("${hotel.bedType.count}, ${hotel.bedType.type}"),
+                        Text(
+                          " ${hotel.bedType.count} ${hotel.bedType.details}",
+                          textDirection: TextDirection.rtl,
+                        ),
+                        const SizedBox(width: 5),
+                        Icon(
+                          Icons.bed,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'از ${formatPrice(hotel.pricePerNight)} / شب',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                  ],
+                ),
+              ),
             ),
-            child: Image.network(
-              networkUrl(hotel.images[0]),
-              width: 100,
-              height: double.infinity,
-              fit: BoxFit.cover,
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+              child: Image.network(
+                networkUrl(hotel.images[0]),
+                width: 100,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
