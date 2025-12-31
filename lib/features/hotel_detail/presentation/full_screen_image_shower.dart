1d680fe8907d85c446e942cf017561d93c775dc4
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-31 17:37:36 +0330

    add FullScreenImageShower

---
+++ lib/features/hotel_detail/presentation/full_screen_image_shower.dart
@@ -0,0 +1,28 @@
+import 'package:Hotelino/core/utils/network.dart';
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+class FullScreenImageShower extends StatefulWidget {
+  final String myImageUrl;
+  const FullScreenImageShower({super.key, required this.myImageUrl});
+
+  @override
+  State<FullScreenImageShower> createState() => _FullScreenImageShowerState();
+}
+
+class _FullScreenImageShowerState extends State<FullScreenImageShower> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: PhotoView(
+          enableRotation: false,
+          initialScale: PhotoViewComputedScale.contained * 1,
+          backgroundDecoration: const BoxDecoration(color: Colors.black),
+          imageProvider: NetworkImage(networkUrl(widget.myImageUrl)),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/hotel_detail/presentation/hotel_detail_screen.dart
+++ lib/features/hotel_detail/presentation/hotel_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:Hotelino/core/utils/network.dart';
 import 'package:Hotelino/features/home/data/models/hotel.dart';
 import 'package:Hotelino/features/home/data/repositories/hotel_repository.dart';
+import 'package:Hotelino/features/hotel_detail/presentation/full_screen_image_shower.dart';
 import 'package:Hotelino/features/hotel_detail/presentation/full_screen_map.dart';
 import 'package:Hotelino/shared/services/json_data_service.dart';
 import 'package:flutter/material.dart';
@@ -34,7 +35,17 @@ class HotelDetailScreen extends StatelessWidget {
                 pinned: false,
                 flexibleSpace: FlexibleSpaceBar(
                   background: GestureDetector(
-                    onLongPress: () {},
+                    onLongPress: () {
+                      PersistentNavBarNavigator.pushNewScreen(
+                        context,
+                        screen: FullScreenImageShower(
+                          myImageUrl: hotel.images.first,
+                        ),
+                        withNavBar: false,
+                        pageTransitionAnimation:
+                            PageTransitionAnimation.cupertino,
+                      );
+                    },
                     child: Image.network(
                       networkUrl(hotel.images.first),
                       fit: BoxFit.cover,
@@ -169,7 +180,17 @@ class HotelDetailScreen extends StatelessWidget {
                             return Row(
                               children: [
                                 GestureDetector(
-                                  onTap: () {},
+                                  onTap: () {
+                                    PersistentNavBarNavigator.pushNewScreen(
+                                      context,
+                                      screen: FullScreenImageShower(
+                                        myImageUrl: hotel.images[index],
+                                      ),
+                                      withNavBar: false,
+                                      pageTransitionAnimation:
+                                          PageTransitionAnimation.cupertino,
+                                    );
+                                  },
                                   child: ClipRRect(
                                     borderRadius: BorderRadius.circular(12),
                                     child: Image.network(
