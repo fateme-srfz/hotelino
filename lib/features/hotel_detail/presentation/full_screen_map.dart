595476e3ccf185ebaeb03eb5e23e70dad1931c35
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-12-31 16:24:08 +0330

    FullScreenMap design finished

--- lib/features/hotel_detail/presentation/full_screen_map.dart
+++ lib/features/hotel_detail/presentation/full_screen_map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class FullScreenMap extends StatelessWidget {
   final double latitude;
@@ -13,6 +15,54 @@ class FullScreenMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(latitude, longitude),
+          initialZoom: 15.0,
+          interactionOptions: const InteractionOptions(
+            flags: InteractiveFlag.all,
+          ),
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            userAgentPackageName: 'ir.dunijet.Hotelino',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(latitude, longitude),
+                width: 80,
+                height: 80,
+                child: Column(
+                  children: [
+                    const Icon(Icons.location_pin, color: Colors.red, size: 40),
+                    Container(
+                      color: Colors.white.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 4,
+                        vertical: 2,
+                      ),
+                      child: Text(
+                        hotelName,
+                        style: textTheme.bodySmall!.copyWith(
+                          color: Colors.black,
+                        ),
+                        textDirection: TextDirection.rtl,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
